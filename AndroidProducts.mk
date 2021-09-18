49b4bb6f1ffddc0e7331c9fa4b78de9705d0317a
SirRGB <liam_wagner@gmx.de>
2021-09-18 15:34:29 +0200

    amy-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/amy_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    amy_dumpling-user \
+    amy_dumpling-userdebug \
+    amy_dumpling-eng
